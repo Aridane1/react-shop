e085cd629886137ca3b9b00bde2577bebbd05ac0
Aridane1 <102683942+H4cks0r6@users.noreply.github.com>
2023-09-08 13:54:03 +0100

    menu animation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,25 +1,17 @@
 import "./header.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
-import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+
 import { useAuth } from "../../context/authContext";
 
 import { useEffect, useState } from "react";
 
-function Header() {
-  const [menu, setMenu] = useState(false);
-  const [menuBars, setMenuBars] = useState(false);
+export default function Header2() {
   const [userLogin, setUserLogin] = useState(false);
   const { user, logout } = useAuth();
 
   const handleLogout = async (e) => {
     await logout();
-  };
-
-  const isOpen = () => {
-    setMenu(!menu);
-    setMenuBars(!menuBars);
   };
 
   const checkIfUserIsLoggedIn = () => {
@@ -35,66 +27,59 @@ function Header() {
 
   useEffect(() => {
     checkIfUserIsLoggedIn();
-  },[user]);
+  }, [user]);
 
   return (
-    <div className="header">
-      <div className="button-menu">
-        <button onClick={isOpen} className="button-nav">
-          <FontAwesomeIcon
-            icon={faBars}
-            className={`faBars ${menuBars ? "isClicked" : ""} `}
-          />
-          <FontAwesomeIcon
-            icon={faXmark}
-            className={`faXmark ${menuBars ? "isClicked" : ""} `}
-          />
-        </button>
-      </div>
-      <div className="header-logo">
-        <img
-          src="/assents/logo/DISTRIBUCIONES-AGRICOLAS.svg"
-          alt="logo"
-          width="150"
-          height="150"
-        />
-      </div>
-      <div className={`header-nav ${menu ? "isActive" : ""} `}>
-        <ul>
-          <li className="header-li">
-            <a href="/home" className="header-a">
-              Home
-            </a>
-            <a href="/products" className="header-a">
-              Productos
-            </a>
-            <a href="/shopping-cart" className="header-a">
-              <FontAwesomeIcon icon={faCartShopping} />
-            </a>
-            <a
-              href="/login"
-              className={`header-a sign-in ${userLogin ? "isLogin" : "isNotLogin"}`}
-            >
-              Login
-            </a>
-            <a
-              href="/register"
-              className={`header-a sign-up ${userLogin ? "isLogin" : "isNotLogin"}`}
-            >
-              SingUp
-            </a>
-            <a
-              href="/home"
-              onClick={handleLogout}
-              className={`header-a sign-out ${userLogin ? "isLogin" : "isNotLogin"}`}
-            >
-              LogOut
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
+    <>
+      <header>
+        <nav class="nav">
+          <div class="nav-container">
+            <h1 class="nav-logo">7 Islas</h1>
+
+            <label htmlFor="menu" class="nav-label">
+              <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAExJREFUSEtjZKAxYKSx+QyjFhAM4QEJov8EnYVfAYqjsfmA5hZQ6AFU7QMSBzT3Ac3jgOYW0DyIhr4FNI8Dmlsw9ONg1AcoIUDz0hQAbegGGXzv/l0AAAAASUVORK5CYII=" />
+            </label>
+
+            <input type="checkbox" id="menu" class="nav-input" />
+            <div class="nav-menu">
+              <a href="/home" className="nav-item">
+                Home
+              </a>
+              <a href="/products" className="nav-item">
+                Productos
+              </a>
+              <a href="/shopping-cart" className="nav-item">
+                <FontAwesomeIcon icon={faCartShopping} />
+              </a>
+              <a
+                href="/login"
+                className={`nav-item sign-in ${
+                  userLogin ? "isLogin" : "isNotLogin"
+                }`}
+              >
+                Login
+              </a>
+              <a
+                href="/register"
+                className={`nav-item sign-up ${
+                  userLogin ? "isLogin" : "isNotLogin"
+                }`}
+              >
+                SingUp
+              </a>
+              <a
+                href="/home"
+                onClick={handleLogout}
+                className={`nav-item sign-out ${
+                  userLogin ? "isLogin" : "isNotLogin"
+                }`}
+              >
+                LogOut
+              </a>
+            </div>
+          </div>
+        </nav>
+      </header>
+    </>
   );
 }
-
-export default Header;
